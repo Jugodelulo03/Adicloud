531b838600138baecf59dcb4c0b6811fa18df58b
Julian Pinzon <est.julian.pinzon@unimilitar.edu.co>
2025-07-27 00:09:14 -0500

    conect back with front in server

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+// configure CORS: allow only your frontend hosted on Render
+const allowedOrigins = ['https://adicloud-hxf9.onrender.com'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // allow request
+    } else {
+      callback(new Error('Not allowed by CORS')); // block request
+    }
+  }
+}));
+
 // import routes
 app.use('/', require('./routes/auth'));
 app.use('/', require('./routes/profile'));
