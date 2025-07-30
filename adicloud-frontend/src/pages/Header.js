693e2d271cb1e83b5947900e52ce2552c7d171ff
Moon <162949341+Lunat91@users.noreply.github.com>
2025-07-30 18:27:29 -0500

    intento de arreglo de header

--- adicloud-frontend/src/pages/Header.js
+++ adicloud-frontend/src/pages/Header.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import LogoC from './assets/adicould.svg';
 import IconProfile from './assets/icon_profile.svg';
 import './dropdownmenuA.css';
 
 const Header = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return(
         <header className="header">
-            <img src={LogoC} alt="Logo" className="logoC" />
-
-            <nav className="menu1">
-                <div className="requestmenu">
-                    <button className= "dropbtn">Requests</button>
-                    <div className="dpdcont">
-                        <a href="/adicloud-frontend\src\pages\AdminDashboard.js">View All</a>
-                        <a href="/">Pending</a>
-                        <a href="/">Approved</a>
-                        <a href="/">Rejected</a>
+            <div className="headerbox">
+                    <img src={LogoC} alt="Logo" className="logoC" />
+                <nav className="menu1">
+                    <div className="requestmenu">
+                        <button className= "dropbtn">Requests</button>
+                        <div className="dpdcont">
+                            <a href="/adicloud-frontend\src\pages\AdminDashboard.js">View All</a>
+                            <a href="/">Pending</a>
+                            <a href="/">Approved</a>
+                            <a href="/">Rejected</a>
+                        </div>
                     </div>
+                    <a href="/" className= "dropbtn gallery"> Gallery</a>
+                </nav>
+                <div className="user-info" ref={dropdownRef}>
+                    <div className="profile-menu" onClick={() => setShowDropdown(!showDropdown)}>
+                        <span>Hi, Maria</span>
+                        <img src={IconProfile} alt="Usuario" className="user-icon" />
+                    </div>
+
+                    {showDropdown && (
+                    <div className="profile-dropdown">
+                        <a href="/adicloud-frontend\src\pages\AdminDashboard.js">Requests</a>
+                        <a href="/">Log Out</a>
+                    </div>
+                    )}
                 </div>
-                <a href="/home" className= "dropbtn gallery"> Gallery</a>
-            </nav>
-            <div className="user-info">
-                <span>Hello, {}</span>
-                <img src={IconProfile} alt="Usuario" className="user-icon"/>
             </div>
         </header>
-    )
+    );
 };
 
 export default Header;
